65369ed68132047e99a4ccabb01bb7b1e0a62bb1
chennu7316 <88182934+chennu7316@users.noreply.github.com>
2023-10-19 17:06:02 +0530

    Create insertNodeAtHead.py

---
+++ insertNodeAtHead.py
@@ -0,0 +1,8 @@
+def insertNodeAtHead(llist, data):
+    # Write your code here
+    if llist==None:
+        llist=data
+        llist.next=None
+    else:
+        new_node.next=list.head
+        list.head=new_node
